673343d034af8337476de7c58f885e1716d821b4
Asanguine <birkan.kurt@proton.me>
2023-10-10 16:43:12 +0200

    simple student add, edit and remove is done

--- service/__init__.py
+++ service/__init__.py
@@ -2,12 +2,12 @@
 Data Models
 """
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://asanguine:15975acfKjfydJ18SQL@asanguine.mysql.pythonanywhere-services.com/asanguine$default'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://asanguine:<PASSWORD>@asanguine.mysql.pythonanywhere-services.com/asanguine$default'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -16,10 +16,10 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 #from service.models import Student, University, Accommodation, StudyArea
-from service.models.student import Student
-from service.models.university import University
-from service.models.accommodation import Accommodation
-from service.models.studyArea import StudyArea
+from service.models.student import Student, University, Accommodation, StudyArea
+# from service.models.university import University
+# from service.models.accommodation import Accommodation
+# from service.models.studyArea import StudyArea
 
 from service import routes
 
@@ -29,9 +29,70 @@ with app.app_context():
     db.create_all()
 
 
+#from service.routes import *
+
+############################################################
+#                       Routes
+############################################################
+
+
 @app.route('/')
 def home():
-    return 'Welcome to the Student Counseling Service!'
+    students = Student.query.all()
+    return redirect(url_for('list_students'))
+
+@app.route('/students/create', methods=['GET', 'POST'])
+def create_student():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        major = request.form.get('major')
+        budget = request.form.get('budget')
+        email = request.form.get('email')
+        phone_number = request.form.get('phone_number')
+        id = Student.query.count() + 1
+
+        student = Student(name=name, major=major, budget=budget)
+        db.session.add(student)
+        db.session.commit()
+
+        return redirect(url_for('list_students'))
+    
+    return render_template('create_student.html')
+
+
+@app.route('/students')
+def list_students():
+    students = Student.query.all()
+    return render_template('list_students.html', students=students)
+
+
+@app.route('/students/<int:id>/edit', methods=['GET', 'POST'])
+def edit_student(id):
+    student = Student.query.get(id)
+
+    if request.method == 'POST':
+        student.name = request.form.get('name')
+        student.major = request.form.get('major')
+        student.income = request.form.get('income')
+
+        db.session.commit()
+        return redirect(url_for('list_students'))
+
+    return render_template('edit_student.html', student=student)
+
+
+@app.route('/students/<int:id>/delete', methods=['POST'])
+def delete_student(id):
+    student = Student.query.get(id)
+
+    db.session.delete(student)
+    db.session.commit()
+
+    return redirect(url_for('list_students'))
+
+
+
+
 
 
 
--- service/models/student.py
+++ service/models/student.py
@@ -1,7 +1,15 @@
 
+import logging
 from flask_sqlalchemy import SQLAlchemy
+from service import db
 
-db = SQLAlchemy()
+logger = logging.getLogger()
+#db = SQLAlchemy()
+
+
+class DataValidationError(Exception):
+    """Used for an data validation errors when deserializing"""
+
 
 ############################################################
 #                     Student Model
@@ -10,14 +18,107 @@ class Student(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100))
+    phone_number = db.Column(db.Integer)
     major = db.Column(db.String(100))
-    income = db.Column(db.Integer)
+    budget = db.Column(db.Integer)
+    status = db.Column(db.String(100))
+    assigned_university = db.Column(db.String(100))
 
     universities = db.relationship('University', secondary='student_university', back_populates='students')
 
+
+    def __repr__(self):
+        return '<Student %r>' % self.name
+
+    def to_dict(self) -> dict:
+        """Serializes the class as a dictionary"""
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def from_dict(self, data: dict) -> None:
+        """Sets attributes from a dictionary"""
+        for key, value in data.items():
+            setattr(self, key, value)
+
+    def create(self):
+        """Creates a Student in the database"""
+        logger.info("Creating %s", self.name)
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """Updates a Student in the database"""
+        logger.info("Saving %s", self.name)
+        if not self.id:
+            raise DataValidationError("Update called with empty ID field")
+        db.session.commit()
+
+    def delete(self):
+        """Removes an Account from the database"""
+        logger.info("Deleting %s", self.name)
+        db.session.delete(self)
+        db.session.commit()
+
+
+############################################################
+#                     StudyArea Model
+############################################################
+class StudyArea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    universities = db.relationship('University', secondary='university_study_area', back_populates='study_areas')
+
+
+
+############################################################
+#                     University Model
+############################################################
+class University(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100))
+
+    students = db.relationship('Student', secondary='student_university', back_populates='universities')
+    study_areas = db.relationship('StudyArea', secondary='university_study_area', back_populates='universities')
+
+
+
+############################################################
+#         student - university relationship                 =================================
+############################################################
 
 student_university = db.Table(
     'student_university',
     db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
     db.Column('university_id', db.Integer, db.ForeignKey('university.id'), primary_key=True)
 )
+
+############################################################
+#         study area - university relationship              =================================
+############################################################
+
+university_study_area = db.Table(
+    'university_study_area',
+    db.Column('university_id', db.Integer, db.ForeignKey('university.id'), primary_key=True),
+    db.Column('study_area_id', db.Integer, db.ForeignKey('study_area.id'), primary_key=True)
+)
+
+
+############################################################
+#                   Accommodation Model
+############################################################
+PREDEFINED_ACCOMMODATION_TYPES = [
+    'Hotel',
+    'Airbnb',
+    'Private Dorm',
+    'School Dorm',
+    'Apartment',
+]
+
+class Accommodation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(50))  # Example: Hotel, Dormitory, etc.
+    price = db.Column(db.Integer)
+    location = db.Column(db.String(100))
